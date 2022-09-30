18aad1c6478984ca36a65d83ec39e9aacaea4c81
Yaroslav Oliinyk <4106677@gmail.com>
2022-09-30 16:41:28 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,30 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 
-console.log('test');
+// console.log(Player);
 
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+player.on('timeupdate', function (e) {
+  let playTime = 0;
+  playTime = e.seconds;
+  localStorage.setItem('videoplayer-current-time', playTime);
+});
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+// localStorage.setItem('videoplayer-current-time', playTime);
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
